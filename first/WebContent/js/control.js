35508395551a9589e9c2185cc6e5e03116b19c46
jimisa <jimisa@jimisa-lap>
2016-07-11 15:58:02 +0200

    First commit

---
+++ first/WebContent/js/control.js
@@ -0,0 +1,14 @@
+/**
+ * http://usejsdoc.org/
+ */
+angular.module('TodoApp',[])
+.controller('TodoCtrl',function($scope) {
+	$scope.todos = [
+	                {text:'learn angular',done:true},
+	                {text:'build an angular app',done:false}
+	                ];
+	$scope.addTodo = function() {
+		$scope.todos.push({text:$scope.TodoText, done:false});
+		$scope.todoText = '';
+	};	
+});
