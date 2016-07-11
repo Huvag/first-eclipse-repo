1ca95518bb5f74114d6358b066c4d28187291102
jimisa <jimisa@jimisa-lap>
2016-07-11 16:19:10 +0200

    nouvelle entrée pour objet todoList

--- first/WebContent/js/control.js
+++ first/WebContent/js/control.js
@@ -5,7 +5,8 @@ angular.module('TodoApp',[])
 .controller('TodoCtrl',function($scope) {
 	$scope.todos = [
 	                {text:'learn angular',done:true},
-	                {text:'build an angular app',done:false}
+	                {text:'build an angular app',done:false},
+	                {text: 'Use test integrattion',done: false}
 	                ];
 	$scope.addTodo = function() {
 		$scope.todos.push({text:$scope.TodoText, done:false});
